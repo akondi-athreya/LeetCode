32f9c8cfbf38f87ea3c1c6c6d43e74ddf15546d9
athreya <119722868+akondi-athreya@users.noreply.github.com>
2024-01-13 14:12:56 +0530

    Time: 82 ms (99.87%) | Memory: 18 MB (11.97%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,14 +1,9 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        lit1=list(s)
-        lit2=list(t)
-        lit3=set(lit1)
-        lit4=set(lit2)
-        print(len(lit3))
-        if(len(lit3)==1 and len(lit4)==1):
-            return len(lit1)
+        res = 0
+
+        for char in set(t):
+            diff = t.count(char) - s.count(char)
+            res += diff if diff > 0 else 0
         
-        for i in lit1:
-            if i in lit2:
-                lit2.remove(i)
-        return len(lit2)
+        return res
