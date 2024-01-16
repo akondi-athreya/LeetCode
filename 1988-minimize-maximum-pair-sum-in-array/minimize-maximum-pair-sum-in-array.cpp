4bad117295d27913102b2f0ffc6f27a83434388b
athreya <119722868+akondi-athreya@users.noreply.github.com>
2024-01-16 22:26:02 +0530

    Time: 196 ms (24.26%) | Memory: 96.7 MB (41.95%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -4,15 +4,17 @@ public:
         sort(nums.begin(),nums.end());
         int i=0;
         int j=nums.size()-1;
-        vector<int> res;
         int sum=0;
+        int max=0;
         while(i<j){
             sum=nums[i]+nums[j];
-            res.push_back(sum);
+            if(sum>max){
+                max=sum;
+            }
             i+=1;
             j-=1;
             sum=0;
         }
-        return *max_element(res.begin(),res.end());
+        return max;
     }
 };
