82843f0cbf533071c08696255c8aeed27296dcf6
athreya <119722868+akondi-athreya@users.noreply.github.com>
2024-02-01 17:08:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (10.83%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -3,6 +3,9 @@ public:
     bool isValid(string s) {
         stack<char> st;
         for(int i=0 ; i<s.size() ; i++){
+            if(st.empty() && (s[i]==')' || s[i]=='}' || s[i]==']')){
+                return false;
+            }
             if(!st.empty() && st.top()=='(' && s[i]==')'){
                 st.pop();
             }
