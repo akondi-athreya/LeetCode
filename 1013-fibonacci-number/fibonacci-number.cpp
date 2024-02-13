30447ee9e2d8267037522effc3991c3e64d36c08
athreya <119722868+akondi-athreya@users.noreply.github.com>
2024-02-13 22:03:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (5.65%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
-    int fsum(int n){
+    /* int fsum(int n){
         if(n<=1){
             return n;
         }
         else{
             return fsum(n-1)+fsum(n-2);
         }
+    } */
+    int fsum(int n , vector<int>&dp){
+        dp[0] = 0;
+        dp[1] = 1;
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        if(dp[n] == -1){
+            fsum(n-1 , dp) + fsum(n-1 , dp);
+        }
+        dp[n] = dp[n-1] + dp[n-2];
+        return dp[n];
     }
     int fib(int n) {
-        return fsum(n);
+        // return fsum(n);
+        if(n==0) return 0;
+        vector<int> dp(n+1,-1);
+        return fsum(n , dp);
     }
 };
