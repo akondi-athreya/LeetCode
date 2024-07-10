31858bc1ffd46af58dbb6520a6c3e8c939c28130
Athreya <119722868+akondi-athreya@users.noreply.github.com>
2024-07-10 19:38:00 +0530

    Time: 3 ms (86.11%) | Memory: 13.9 MB (61.11%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int ans = 0;
+        for(string &i : logs){
+            if(i == "../") ans = max(0,ans-1);
+            else if(i == "./") continue;
+            else ans++;
+        }
+        return ans;
+    }
+};
