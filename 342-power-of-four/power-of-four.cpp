78effb25a1374bf98a1aefe7a5a810c437f84cee
Athreya <119722868+akondi-athreya@users.noreply.github.com>
2024-10-08 11:44:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (88.34%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfFour(int n) {
         if(n==0) return false;
         if(n==1) return true;
-        if(n%4==0) return isPowerOfFour(n/4);
+        if(n%4==0) return isPowerOfFour(n>>2);
         else return false;
     }
 };
