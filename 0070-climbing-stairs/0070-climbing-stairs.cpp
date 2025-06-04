49118e688fe7a3c7d46bc934a8dfcdf66ec441a7
Athreya <119722868+akondi-athreya@users.noreply.github.com>
2025-06-04 10:28:39 +0530

    06-04-2025 0070-climbing-stairs Easy .cpp

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1, 0);
-        dp[0] = 0;
-        dp[1] = 1;
-        if(n > 1) dp[2] = 2;
-        if(n <= 2) return dp[n];
-        for(int i=3 ; i<=n ; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        int a = 1;
+        int b = 2;
+        int curr;
+        for(int i = 3; i <= n; ++i) {
+            curr = a + b;
+            a = b;
+            b = curr;
         }
-        return dp[n];
+        return b;
     }
 };
