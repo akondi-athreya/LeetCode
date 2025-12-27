391af31ab399a00cfa2805580f137c2b625e9a57
Athreya <22MH1A42E7@acoe.edu.in>
2025-12-27 22:33:52 +0530

    Time: 12 ms (31.91%), Space: 44.8 MB (74.68%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n = g.size(), m = s.size();
+        int i = n-1, j = m-1;
         sort(begin(g), end(g));
         sort(begin(s), end(s));
-        int i=0 , j=0;
-        while(i < g.size() && j < s.size()) {
-            if(s[j] >= g[i]) {
-                i++;
+
+        int cnt = 0;
+
+        while (i >= 0 && j >= 0) {
+            if (g[i] <= s[j]) {
+                cnt++;
+                // cout << "i : " << i << " , j : " << j << "\n";
+                i--;
+                j--;
             }
-            j++;
+            else {
+                i--;
+            }
         }
-        return i;
+
+        return cnt;
     }
 };
+
+// |
+// 2 3 - i
+// 1 2 - j
