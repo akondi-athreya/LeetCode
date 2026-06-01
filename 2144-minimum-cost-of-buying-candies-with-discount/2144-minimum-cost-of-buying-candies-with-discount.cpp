18e50de926deb00c6c49bef6bd5760fa483ddd28
Athreya <22MH1A42E7@acoe.edu.in>
2026-06-01 21:19:39 +0530

    Time: 3 ms (12.55%), Space: 17 MB (7.87%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(rbegin(cost), rend(cost));
+        int n = cost.size();
+        int ans = 0;
+        for (int i = 0 ; i < n ; i++) {
+            if ((i+1) % 3 == 0) continue;
+            ans += cost[i];
+        }
+        return ans;
+    }
+};
