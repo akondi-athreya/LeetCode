bff9db12d9900074e112d70ded13c1a739dcfb33
Athreya <22MH1A42E7@acoe.edu.in>
2026-06-16 05:34:37 +0530

    Time: 3 ms (76.89%) | Memory: 39.6 MB (87.50%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string ans = "";
+        for (char ch : s) {
+            if (ch >= 'a' && ch <= 'z') ans += ch;
+            else if (ch == '#') {
+                ans += ans;
+            }
+            else if (ch == '%') {
+                if (!ans.empty()) reverse(ans.begin(), ans.end());
+            }
+            else if (ch == '*') {
+                if (!ans.empty()) ans.pop_back();
+            }
+        }
+        return ans;
+    }
+};
