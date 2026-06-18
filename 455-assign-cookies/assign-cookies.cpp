fa0eb83879dd85f7a662af00f855dced48d6a552
Athreya <22MH1A42E7@acoe.edu.in>
2026-06-18 06:22:18 +0530

    Time: 7 ms (70.39%) | Memory: 44.9 MB (11.45%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n = g.size(), m = s.size();
+        int i = n-1, j = m-1;
+        sort(begin(g), end(g));
+        sort(begin(s), end(s));
+
+        int cnt = 0;
+
+        while (i >= 0 && j >= 0) {
+            if (g[i] <= s[j]) {
+                cnt++;
+                i--;
+                j--;
+            }
+            else {
+                i--;
+            }
+        }
+
+        return cnt;
+    }
+};
