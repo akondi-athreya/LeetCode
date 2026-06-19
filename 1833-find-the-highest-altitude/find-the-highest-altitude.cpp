33292f1a584a56fbe7fe8ecd9d11362ed90caa13
Athreya <22MH1A42E7@acoe.edu.in>
2026-06-19 05:46:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (44.68%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        vector<int> pre;
-        pre.emplace_back(0);
-        for(int i=0 ; i<gain.size() ; i++){
-            int sum = pre[i]+gain[i];
-            pre.emplace_back(sum);
+        int curr = 0;
+        int mx = 0;
+        for (int it : gain) {
+            curr += it;
+            mx = max(mx, curr);
         }
-        return *max_element(pre.begin(),pre.end());
+        return mx;
     }
 };
