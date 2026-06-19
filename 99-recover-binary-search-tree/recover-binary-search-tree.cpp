542c976bd9e62a6abcfb9818a4a79c32f940c4f4
Athreya <22MH1A42E7@acoe.edu.in>
2026-06-19 05:36:35 +0530

    Time: 0 ms (100.00%) | Memory: 61 MB (17.06%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* first;
+    TreeNode* last;
+    TreeNode* middle;
+    TreeNode* prev;
+    void fun(TreeNode* root) {
+        if (root == NULL) return;
+        fun(root->left);
+
+        if (prev != NULL && (root->val < prev->val)) {
+            if (first == NULL) {
+                first = prev;
+                middle = root;
+            }
+            else last = root;
+        }
+
+        prev = root;
+        fun(root->right);
+    }
+    void recoverTree(TreeNode* root) {
+        first = middle = last = NULL;
+        prev = new TreeNode(INT_MIN);
+        fun(root);
+        if (first && last) swap(first->val, last->val);
+        else if (first && middle) swap(first->val, middle->val);
+    }
+};
