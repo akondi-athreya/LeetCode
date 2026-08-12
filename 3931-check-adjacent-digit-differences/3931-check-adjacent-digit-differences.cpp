69902412ebb6dbea5d0759e502524c731c09fe81
Athreya <22MH1A42E7@acoe.edu.in>
2026-08-12 21:50:15 +0530

    Time: 0 ms (100%), Space: 8.7 MB (46.04%) - LeetHub

---
+++ 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        int n = s.size();
+        for (int i = 0 ; i < n-1 ; i++) {
+            if (abs((s[i] - '0') - (s[i+1] - '0')) > 2) return false;
+        }
+        return true;
+    }
+};
