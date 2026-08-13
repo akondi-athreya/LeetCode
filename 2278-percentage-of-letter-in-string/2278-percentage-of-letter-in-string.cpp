53cd9d288725bc5477055cf75602b68390bc43fc
Athreya <22MH1A42E7@acoe.edu.in>
2026-08-13 11:50:08 +0530

    Time: 0 ms (100%), Space: 8.1 MB (92.23%) - LeetHub

---
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int n = s.size();
+        int cnt = 0;
+        for (char ch : s) {
+            if (ch == letter) cnt++;
+        }
+        return (cnt*100)/n;
+    }
+};
